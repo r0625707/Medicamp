8e167c99fb4b240153e818ad8c3cea231bd4a3cf
r0625707 <bram.vanasschodt@student.ucll.be>
2018-02-13 20:51:15 +0100

    back-end deployed on appengine

--- src/main/java/medicamp/api/HelloworldController.java
+++ src/main/java/medicamp/api/HelloworldController.java
@@ -22,6 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldController {
   @GetMapping("/")
   public String hello() {
-    return "Hello world - springboot-appengine-standard!";
+    return "Welcome to Medicamp API!";
   }
 }
