42573b015eca4af6f8d8b5d102831db01768bb19
Michiel <michiel.vh@hotmail.com>
2018-02-15 04:33:11 +0100

    start JPA -- persistence.xml en @entity nog aan klassen toevoegen

--- src/main/java/medicamp/database/Database.java
+++ src/main/java/medicamp/database/Database.java
@@ -3,9 +3,33 @@ package medicamp.database;
 import medicamp.model.Voogd;
 
 public class Database {
-public void addVoogd(Voogd voogd) {
-	voogd.getBus();
-	voogd.getEmail();
-	voogd.getHuisNummer();
-}
+//	private EntityManagerFactory factory;
+//	private EntityManager manager;
+//	private String DBNAME;
+//	
+//	public void openConnection(String name){
+//		factory = Persistence.createEntityManagerFactory(name);
+//		manager = factory.createEntityManager();
+//		}
+//	
+//		public void closeConnection() throws DatabaseException {
+//		try {
+//		manager.close();
+//		factory.close();
+//		} catch (Exception e) {
+//		throw new DatabaseException(e.getMessage(), e);
+//		}
+//		}
+//	
+//public void addVoogd(Voogd voogd) {
+//	manager.getTransaction().begin();
+//	manager.persist(voogd);
+//	manager.getTransaction().commit();
+//}
+//public Voogd getVoogd(Long id) {
+//Voogd voogd = manager.find(Voogd.class, id);
+//return voogd;
+//}
+
+
 }
