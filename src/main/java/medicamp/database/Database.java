f9027f62a43b8b215378f7aaf92aad460cb829c6
Michiel <michiel.vh@hotmail.com>
2018-02-15 05:09:43 +0100

    klein fixke, persistence.xml nog fixen

--- src/main/java/medicamp/database/Database.java
+++ src/main/java/medicamp/database/Database.java
@@ -1,35 +1,39 @@
 package medicamp.database;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import medicamp.model.Voogd;
 
 public class Database {
-//	private EntityManagerFactory factory;
-//	private EntityManager manager;
-//	private String DBNAME;
-//	
-//	public void openConnection(String name){
-//		factory = Persistence.createEntityManagerFactory(name);
-//		manager = factory.createEntityManager();
-//		}
-//	
-//		public void closeConnection() throws DatabaseException {
-//		try {
-//		manager.close();
-//		factory.close();
-//		} catch (Exception e) {
-//		throw new DatabaseException(e.getMessage(), e);
-//		}
-//		}
-//	
-//public void addVoogd(Voogd voogd) {
-//	manager.getTransaction().begin();
-//	manager.persist(voogd);
-//	manager.getTransaction().commit();
-//}
-//public Voogd getVoogd(Long id) {
-//Voogd voogd = manager.find(Voogd.class, id);
-//return voogd;
-//}
+	private EntityManagerFactory factory;
+	private EntityManager manager;
+	private String dbName;
+	
+	public void openConnection(String name){
+		factory = Persistence.createEntityManagerFactory(name);
+		manager = factory.createEntityManager();
+		}
+	
+		public void closeConnection() throws Exception {
+		try {
+		manager.close();
+		factory.close();
+		} catch (Exception e) {
+		throw new Exception(e.getMessage(), e);
+		}
+		}
+	
+public void addVoogd(Voogd voogd) {
+	manager.getTransaction().begin();
+	manager.persist(voogd);
+	manager.getTransaction().commit();
+}
+public Voogd getVoogd(Long id) {
+Voogd voogd = manager.find(Voogd.class, id);
+return voogd;
+}
 
 
 }
