be9cffe82c1802a1ca455a569bc2df7168d2d63f
r0625707 <bram.vanasschodt@student.ucll.be>
2018-02-24 17:02:52 +0100

    cross origins voor localhost:3000 (web-app)

--- src/main/java/com/medicamp/api/UserController.java
+++ src/main/java/com/medicamp/api/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.medicamp.model.Voogd;
 import com.medicamp.db.UserRepository;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 @RequestMapping("/api/user")
 public class UserController {
 
