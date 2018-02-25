b6bb46126c8f868f9c9d63c2fae4fce63255a419
r0625707 <bram.vanasschodt@student.ucll.be>
2018-02-25 12:07:18 +0100

    GroepController, basic validation

---
+++ src/main/java/com/medicamp/db/TakRepository.java
@@ -0,0 +1,9 @@
+package com.medicamp.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medicamp.model.Tak;
+
+public interface TakRepository extends JpaRepository<Tak, String>{
+
+}
