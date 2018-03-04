1a4767e2fe0f4b4fdcbd52b55372f90c1cfcb292
r0625707 <bram.vanasschodt@student.ucll.be>
2018-03-04 22:25:56 +0100

    authorization header

--- src/main/java/com/medicamp/sec/JWTAuthorisationFilter.java
+++ src/main/java/com/medicamp/sec/JWTAuthorisationFilter.java
@@ -38,6 +38,7 @@ public class JWTAuthorisationFilter extends BasicAuthenticationFilter {
 	        }
 	        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
 	        SecurityContextHolder.getContext().setAuthentication(authentication);
+	        res.setHeader("Access-Control-Expose-Header", "Authorization");
 	        chain.doFilter(req, res);
 	    }
 	 
