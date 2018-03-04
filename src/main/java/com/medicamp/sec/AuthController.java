e40d82fda8b59e03ddc9864f414423ce2e94bd03
r0625707 <bram.vanasschodt@student.ucll.be>
2018-03-04 20:22:29 +0100

    cors for localhost:3000

--- src/main/java/com/medicamp/sec/AuthController.java
+++ src/main/java/com/medicamp/sec/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.medicamp.db.UserRepository;
 import com.medicamp.model.User;
 
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/auth/")
 public class AuthController {
